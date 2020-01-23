1da175d0cdefe135a502a67f29f2efe3b3aaed99
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-01-23 22:09:42 +0530

    added console logger

---
+++ saberIosApp/saberIosApp/Sources/ConsoleLogger.swift
@@ -0,0 +1,15 @@
+//
+//  ConsoleLogger.swift
+//  saberIosApp
+//
+//  Created by sunil.kumar1 on 1/23/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+class ConsoleLogger {
+    func log(message: String) {
+        print("\(message)")
+    }
+}
