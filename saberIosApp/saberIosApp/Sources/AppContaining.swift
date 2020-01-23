31a5bdc310e4e16a4b89cf16d77ede81cc4dc061
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-01-23 21:39:15 +0530

    added saber

---
+++ saberIosApp/saberIosApp/Sources/AppContaining.swift
@@ -0,0 +1,12 @@
+//
+//  AppContaining.swift
+//  saberIosApp
+//
+//  Created by sunil.kumar1 on 1/23/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+// @saber.container(AppContainer)
+// @saber.scope(App)
+protocol AppContaining {}
