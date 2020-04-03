881ed13ea1890a4a78ca85d67bfc10fa140fbf96
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-04-03 22:12:19 +0530

    brief container

---
+++ saberIosApp/saberIosApp/brief/brief/Brief.swift
@@ -0,0 +1,15 @@
+//
+//  Brief.swift
+//  saberIosApp
+//
+//  Created by sunil.kumar1 on 4/3/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+// @saber.container(briefContainer)
+// @saber.scope(brief)
+protocol brief {
+    
+}
