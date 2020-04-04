107c6e74562636e524d7b6f5c5895530d68cd652
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-04-04 19:14:18 +0530

    container injection

---
+++ saberIosApp/saberIosApp/ContainerInjection/CIFoo.swift
@@ -0,0 +1,16 @@
+//
+//  CIFoo.swift
+//  saberIosApp
+//
+//  Created by sunil.kumar1 on 4/4/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+// @saber.scope(containerInjection)
+class CIFoo {
+    
+    // @saber.inject
+    var container : containerInjectionContainer!
+}
---
+++ saberIosApp/saberIosApp/ContainerInjection/ContainerInjectionContaining.swift
@@ -0,0 +1,15 @@
+//
+//  ContainerInjectionContaining.swift
+//  saberIosApp
+//
+//  Created by sunil.kumar1 on 4/4/20.
+//  Copyright © 2020 sunil.kumar1. All rights reserved.
+//
+
+import Foundation
+
+// @saber.scope(containerInjection)
+// @saber.container(containerInjectionContainer)
+protocol ContainerInjectionContaining : ContainerInjectionBaseContaining {
+    
+}
